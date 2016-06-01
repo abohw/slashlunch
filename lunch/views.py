5e345bcea80a8908642c1ee51741d44342dc4174
Alex Bowman <alex@bohw.net>
2016-06-01 15:50:08 -0400

    initial foursquare integration

--- lunch/views.py
+++ lunch/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+import foursquare
+from django.conf import settings
+from pprint import pprint
 
 # Create your views here.
 
@@ -21,7 +24,11 @@ def lunch(request):
     text=94070
     response_url=https://hooks.slack.com/commands/1234/5678
     '''
-
+    client = foursquare.Foursquare(client_id=settings.FS_CLIENT_ID, client_secret=settings.FS_CLIENT_SECRET)
     token = request.POST.get('token')
+    venue = client.venues('40a55d80f964a52020f31ee3')
+    something = venue['venue']['name']
+    print something
+#    pprint(venue)
     text = 'temp'
     return HttpResponse(token)
