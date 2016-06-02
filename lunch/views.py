528c5267984d6dc0928d7f0ffb8159abb9b867e1
Alex Bowman <alex@bohw.net>
2016-06-01 21:12:02 -0400

    ok really fixed it this time

--- lunch/views.py
+++ lunch/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.http import HttpResponseForbidden
 import foursquare
 from django.conf import settings
 from pprint import pprint
-from django.http import HttpResponseForbidden
 import random
 
 # Create your views here.
@@ -17,43 +17,43 @@ import random
 def lunch(request):
 
     client = foursquare.Foursquare(client_id=settings.FS_CLIENT_ID, client_secret=settings.FS_CLIENT_SECRET)
-
     slacktoken = request.POST.get('token')
 
-    if slacktoken is settings.SLACK_KEY:
+    venues = client.venues.explore(params={
+    'll': '39.1015337,-84.5173639',
+    'radius': '1750',
+    'section': 'food',
+    'price': '1,2',
+    'openNow': '1'})
 
-        venues = client.venues.explore(params={
-        'll': '39.1015337,-84.5173639',
-        'radius': '1750',
-        'section': 'food',
-        'price': '1,2',
-        'openNow': '1'})
+#    pprint(venue)
 
-        places = []
-        recs = "Hi there! :wave: \n"
+    places = []
+    recs = "Hi there! :wave: \n"
 
-        for groups in venues['groups']:
-            for items in groups['items']:
+    for groups in venues['groups']:
+        for items in groups['items']:
 
-                try:
-                    url = items['venue']['menu']['url']
-                except KeyError:
-                    url = items['venue']['url']
-                except IndexError:
-                    pass
+            try:
+                url = items['venue']['menu']['url']
+            except KeyError:
+                url = items['venue']['url']
+            except IndexError:
+                pass
 
-                places.append([items['venue']['name'],url])
+            places.append([items['venue']['name'],url])
 
-        choices = random.sample(places, 5)
+    choices = random.sample(places, 5)
 
-        for index, name in enumerate(choices):
-            if index == 0: emoji = ":one: "
-            if index == 1: emoji = ":two: "
-            if index == 2: emoji = ":three: "
-            if index == 3: emoji = ":four: "
-            if index == 4: emoji = ":five: "
-            recs += '%s %s \n%s \n' % (emoji, name[0], name[1])
+# menu url
 
-        return JsonResponse({"response_type": "in_channel", "text": recs})
+    for index, name in enumerate(choices):
+        if index == 0: emoji = ":one: "
+        if index == 1: emoji = ":two: "
+        if index == 2: emoji = ":three: "
+        if index == 3: emoji = ":four: "
+        if index == 4: emoji = ":five: "
+        recs += '%s %s \n%s \n' % (emoji, name[0], name[1])
 
+    if slacktoken == settings.SLACK_KEY: return JsonResponse({"response_type": "in_channel", "text": recs})
     else: return HttpResponseForbidden()
