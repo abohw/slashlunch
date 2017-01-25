92f895120282e575bf242f90bde9fc97946a318a
Alex Bowman <alex@bohw.net>
2017-01-25 17:26:15 -0500

    expanding search area for specific searches

--- lunch/views.py
+++ lunch/views.py
@@ -48,7 +48,7 @@ def lunch(request):
         recs = "Hi there! :wave: Here are some lunch options:\n"
         venues = client.venues.explore(params={
         'll': '39.1015337,-84.5173639',
-        'radius': '2900',
+        'radius': '1750',
         'section': 'food',
         'price': '1,2',
         'openNow': '1'})
@@ -56,7 +56,7 @@ def lunch(request):
         recs = "Hi there! :wave: Here are some options with %s:\n" % (slackcopy)
         venues = client.venues.explore(params={
         'll': '39.1015337,-84.5173639',
-        'radius': '1750',
+        'radius': '2900',
         'query': slackcopy,
         'openNow': '1'})
 
