345a13813988651e47367af63e442ea3ac075368
Alex Bowman <alex@bohw.net>
2017-03-15 15:09:21 -0400

    ok now fixing css

--- slashlunch/settings.py
+++ slashlunch/settings.py
@@ -132,5 +132,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = ''
-STATIC_URL = '/static/'
+STATIC_URL = '~/Env/slash-lunch/lib/python2.7/site-packages/django/contrib/admin/static/admin'
