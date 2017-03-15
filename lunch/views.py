e3c712c80114b3bbe625e0cc76bd3c83fb8ef7e9
Alex Bowman <alex@bohw.net>
2017-03-15 15:35:53 -0400

    fixing return for unapproved slacks

--- lunch/views.py
+++ lunch/views.py
@@ -23,7 +23,7 @@ def lunch(request):
     slacktoken = request.POST.get('token')
     slackcopy = request.POST.get('text')
 
-    company = company.objects.get(key__exact=slacktoken)
+    company = Company.objects.get(key__exact=slacktoken)
 
     office = company.office
     radius = company.radius
